e0feeb595f7047d44f8118c2163bb151791e6490
hare <zhangyixuan@shanlitech.com>
2026-04-09 14:46:35 +0800

    新增 普通函数中使用的多语言模块

--- src/i18n/data.ts
+++ src/i18n/data.ts
@@ -1,3 +1,5 @@
+import { createIntl } from "react-intl";
+import { Atom, store } from "#/store";
 import { default as en } from "./json/en.json";
 import { default as zhCN } from "./json/zh-CN.json";
 
@@ -8,3 +10,14 @@ export const messages: Record<string, typeof zhCN> = {
 	en,
 	"zh-CN": zhCN,
 };
+/**
+ * 获取intl实例
+ */
+export function getIntl() {
+	const language = store.get(Atom.language);
+
+	return createIntl({
+		locale: language,
+		messages: messages[language],
+	});
+}
