41a017426dd2cc49bf472092a490899152ddb6d4
hare <zhangyixuan@shanlitech.com>
2026-04-09 16:31:23 +0800

    新增 切换语言原子

--- src/store/atom/data.ts
+++ src/store/atom/data.ts
@@ -5,3 +5,11 @@ import { atom } from "jotai";
  * @default navigator.language
  */
 export const language = atom(navigator.language);
+/**
+ * 切换语言
+ */
+export const switchLanguage = atom(language, (get, set) => {
+	const _lang = get(language);
+
+	set(language, _lang === "zh-CN" ? "en" : "zh-CN");
+});
